5da24fbc0561ef21f631a4a49c2e22af4b818682
Danilo Pianini <danilo.pianini@unibo.it>
2015-02-24 11:17:03 +0100

    Switch ReflectionUtils back to classic

--- src/main/java/it/unibo/alchemist/language/protelis/util/ReflectionUtils.java
+++ src/main/java/it/unibo/alchemist/language/protelis/util/ReflectionUtils.java
@@ -18,9 +18,8 @@ import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
-import org.danilopianini.lang.Pair;
+import org.apache.commons.math3.util.Pair;
 import org.danilopianini.lang.PrimitiveUtils;
 
 /**
@@ -76,53 +75,64 @@ public final class ReflectionUtils {
 	 * @return the result of the invocation, or an {@link IllegalStateException}
 	 *         if something goes wrong.
 	 */
-	public static Object invokeBestMethod(final Class<?> clazz, final String methodName, final Object target, final Object[] args) {
+	public static Method searchBestMethod(final Class<?> clazz, final String methodName, final Object target, final Object[] args) {
 		Objects.requireNonNull(clazz, "The class on which the method will be invoked can not be null.");
 		Objects.requireNonNull(methodName, "Method name can not be null.");
 		Objects.requireNonNull(args, "Method arguments can not be null.");
-		final Stream<Method> candidates = Arrays.stream(clazz.getMethods())
-				.filter(m -> m.getParameterCount() == args.length && methodName.equals(m.getName()));
 		/*
 		 * Deal with Java method overloading scoring methods
 		 */
-		final Optional<Method> best = candidates.collect(
-				/*
-				 * Filter and score methods
-				 */
-				() -> new ArrayList<Pair<Integer, Method>>(),
-				(l, m) -> {
-					final Class<?>[] params = m.getParameterTypes();
-					int p = 0;
-					for (int i = 0; i < args.length; i++) {
-						final Class<?> expected = params[i];
-						final Object actual = args[i];
-						if (expected.isAssignableFrom(actual.getClass())) {
-							/*
-							 * No downcast required, there is compatibility
-							 */
-							p++;
-						} else if (!PrimitiveUtils.classIsNumber(expected)) {
-							/*
-							 * No downcast possible, incompatible method.
-							 */
-							return;
-						}
+		final Method[] candidates = clazz.getMethods();
+		final List<Pair<Integer, Method>> lm = new ArrayList<>(candidates.length);
+		for (final Method m : candidates) {
+			if (m.getParameterCount() == args.length && methodName.equals(m.getName())) {
+				final Class<?>[] params = m.getParameterTypes();
+				int p = 0;
+				boolean compatible = true;
+				for (int i = 0; compatible && i < args.length; i++) {
+					final Class<?> expected = params[i];
+					final Object actual = args[i];
+					if (expected.isAssignableFrom(actual.getClass())) {
+						/*
+						 * No downcast required, there is compatibility
+						 */
+						p++;
+					} else if (!PrimitiveUtils.classIsNumber(expected)) {
+						compatible = false;
 					}
-					l.add(new Pair<>(p, m));
-				},
-				List::addAll).stream()
-				/*
-				 * Find best
-				 */
-				.max((pm1, pm2) -> pm1.getFirst().compareTo(pm2.getFirst()))
-				.map(Pair::getSecond);
+				}
+				if (compatible) {
+					lm.add(new Pair<>(p, m));
+				}
+			}
+		}
+		/*
+		 * Find best
+		 */
+		final Optional<Method> best = lm.stream().max((pm1, pm2) -> pm1.getFirst().compareTo(pm2.getFirst())).map(Pair::getSecond);
 		if (best.isPresent()) {
-			return invokeMethod(best.get(), target, args);
+			return best.get();
 		}
 		final String argType = Arrays.stream(args).map(Object::getClass).collect(Collectors.toList()).toString();
-		throw new NoSuchMethodError(methodName + "/" + args.length + argType + " does not exist in class " + clazz);
+		throw new NoSuchMethodError(methodName + "/" + args.length + argType + " does not exist in " + clazz);
 	}
 	
+	/**
+	 * @param clazz
+	 *            the class where to search for suitable methods
+	 * @param methodName
+	 *            the method to be invoked
+	 * @param target
+	 *            the target object. It can be null, if the method which is
+	 *            being invoked is static
+	 * @param args
+	 *            the arguments for the method
+	 * @return the result of the invocation, or an {@link IllegalStateException}
+	 *         if something goes wrong.
+	 */
+	public static Object invokeBestMethod(final Class<?> clazz, final String methodName, final Object target, final Object[] args) {
+		return invokeMethod(searchBestMethod(clazz, methodName, target, args), target, args);
+	}
 	/**
 	 * @param method
 	 *            the methods to invoke
