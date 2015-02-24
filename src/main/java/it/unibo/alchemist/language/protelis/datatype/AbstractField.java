585a9aa61a06ea45dbaa7690d0704b08076bc69d
Danilo Pianini <danilo.pianini@unibo.it>
2015-02-24 11:11:30 +0100

    Javadoc and style fixes

--- src/main/java/it/unibo/alchemist/language/protelis/datatype/AbstractField.java
+++ src/main/java/it/unibo/alchemist/language/protelis/datatype/AbstractField.java
@@ -17,22 +17,51 @@ import it.unibo.alchemist.model.interfaces.INode;
 import org.danilopianini.lang.HashUtils;
 import org.danilopianini.lang.Pair;
 
+/**
+ * @author Danilo Pianini
+ *
+ */
 public abstract class AbstractField implements Field {
 
 	private static final long serialVersionUID = 7507440716878809781L;
 
+	@Override
 	public INode<Object> reduceKeys(final BinaryOperator<INode<Object>> op, final INode<Object> exclude) {
 		return reduce(nodeIterator(), op, exclude, null);
 	}
 	
+	@Override
 	public Object reduceVals(final BinaryOperator<Object> op, final INode<Object> exclude, final Object defaultVal) {
 		return reduce(valIterator(), op, exclude == null ? null : getSample(exclude), defaultVal);
 	}
 	
+	@Override
 	public Pair<INode<Object>, Object> reducePairs(final BinaryOperator<Pair<INode<Object>, Object>> accumulator, final INode<Object> exclude) {
 		return reduce(coupleIterator(), accumulator, exclude == null ? null : new Pair<>(exclude, getSample(exclude)), null);
 	}
 	
+	/**
+	 * Reduce the desired {@link Iterable} using the provided
+	 * {@link BinaryOperator}, excluding an element and returning a default
+	 * value if the {@link Iterable} is empty or only contains the value to
+	 * exclude. If multiple elements to exclude are present in the iterable,
+	 * only the first will be discarded.
+	 * 
+	 * @param c
+	 *            the {@link Iterable}
+	 * @param op
+	 *            the {@link BinaryOperator} that selects which element should
+	 *            be kept
+	 * @param exclude
+	 *            element not to consider (can be null)
+	 * @param defaultVal
+	 *            the value to return if the iterator is empty or only contains
+	 *            the value to ignore
+	 * @param <T>
+	 *            return type
+	 * @return the reduced element
+	 * 
+	 */
 	protected static <T> T reduce(final Iterable<T> c, final BinaryOperator<T> op, final T exclude, final T defaultVal) {
 		Objects.requireNonNull(c);
 		Objects.requireNonNull(op);
@@ -55,7 +84,7 @@ public abstract class AbstractField implements Field {
 	@Override
 	public String toString() {
 		final StringBuilder sb = new StringBuilder("[ ");
-		for(final Pair<INode<Object>, Object> entry: coupleIterator()) {
+		for (final Pair<INode<Object>, Object> entry : coupleIterator()) {
 			sb.append(entry);
 			sb.append(' ');
 		}
@@ -65,14 +94,14 @@ public abstract class AbstractField implements Field {
 	
 	@Override
 	public boolean equals(final Object o) {
-		if(HashUtils.pointerEquals(this, o)) {
+		if (HashUtils.pointerEquals(this, o)) {
 			return true;
 		}
-		if(o instanceof Field) {
+		if (o instanceof Field) {
 			final Field cmp = (Field) o;
 			if (cmp.size() == size()) {
-				for(final Pair<INode<Object>, Object> pv: coupleIterator()) {
-					if( !pv.getSecond().equals(cmp.getSample(pv.getFirst()))) {
+				for (final Pair<INode<Object>, Object> pv : coupleIterator()) {
+					if (!pv.getSecond().equals(cmp.getSample(pv.getFirst()))) {
 						return false;
 					}
 				}
@@ -85,8 +114,8 @@ public abstract class AbstractField implements Field {
 	@Override
 	public int hashCode() {
 		int[] hash = new int[size()];
-		int i=0;
-		for(final Pair<INode<Object>, Object> pv: coupleIterator()) {
+		int i = 0;
+		for (final Pair<INode<Object>, Object> pv : coupleIterator()) {
 			hash[i++] = pv.hashCode();
 		}
 		return HashUtils.djb2int32(hash);
