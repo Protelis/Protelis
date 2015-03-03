e273f18a21b010f4723e5758ef6a9e1adbc5d3dc
Danilo Pianini <danilo.pianini@unibo.it>
2015-03-03 11:51:40 +0100

    Fix field applicability of Op3

--- src/main/java/it/unibo/alchemist/language/protelis/Op3.java
+++ src/main/java/it/unibo/alchemist/language/protelis/Op3.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2010-2014, Danilo Pianini and contributors
+ * Copyright (C) 2010-2015, Danilo Pianini and contributors
  * listed in the project's pom.xml file.
  * 
  * This file is part of Alchemist, and is distributed under the terms of
@@ -9,10 +9,13 @@
 package it.unibo.alchemist.language.protelis;
 
 
+import gnu.trove.list.TIntList;
+import gnu.trove.list.array.TIntArrayList;
 import it.unibo.alchemist.language.protelis.datatype.Field;
 
 import java.util.Arrays;
 import java.util.Map;
+import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
 
 import org.danilopianini.lang.TriFunction;
@@ -23,12 +26,11 @@ import org.danilopianini.lang.TriFunction;
  */
 public enum Op3 {
 	
+	/**
+	 * Functional mux.
+	 */
 	MUX("mux", Op3::mux);
 	
-	private static final int[] BOTH = new int[]{0,1};
-	private static final int[] LEFT = new int[]{0};
-	private static final int[] RIGHT = new int[]{1};
-	private static final int[] NONE = new int[]{};
 	private static final Map<String, Op3> MAP = new ConcurrentHashMap<>();
 	private final TriFunction<Object, Object, Object, Object> fun;
 	private final String n;
@@ -38,12 +40,29 @@ public enum Op3 {
 		n = name;
 	}
 	
+	/**
+	 * @param a
+	 *            first argument
+	 * @param b
+	 *            second argument
+	 * @param c
+	 *            third argument
+	 * @return the function applied to the three arguments. Any of them can be a
+	 *         {@link Field}.
+	 */
 	public Object run(final Object a, final Object b, final Object c) {
-		final boolean afield = a instanceof Field;
-		final boolean bfield = b instanceof Field;
-		final int[] fields = afield && bfield ? BOTH : afield ? LEFT : bfield? RIGHT : NONE;
-		if(fields.length > 0) {
-			return Field.apply(fun, fields, a, b, c);
+		final Object[] args = {a, b, c};
+		Optional<TIntList> idx = Optional.empty();
+		for (int i = 0; i < args.length; i++) {
+			if (args[i] instanceof Field) {
+				if (!idx.isPresent()) {
+					idx = Optional.of(new TIntArrayList(3));
+				}
+				idx.get().add(i);
+			}
+		}
+		if (idx.isPresent()) {
+			return Field.apply(fun, idx.get().toArray(), a, b, c);
 		}
 		return fun.apply(a, b, c);
 	}
@@ -53,9 +72,13 @@ public enum Op3 {
 		return n;
 	}
 
+	/**
+	 * @param name the operation name
+	 * @return the corresponding {@link Op3}
+	 */
 	public static Op3 getOp(final String name) {
 		Op3 op = MAP.get(name);
-		if(op == null) {
+		if (op == null) {
 			op = Arrays.stream(values()).parallel().filter(o -> o.n.equals(name)).findFirst().get();
 			MAP.put(name, op);
 		}
@@ -63,13 +86,13 @@ public enum Op3 {
 	}
 	
 	private static Object mux(final Object a, final Object b, final Object c) {
-		if(a instanceof Boolean) {
-			if((Boolean) a) {
+		if (a instanceof Boolean) {
+			if ((Boolean) a) {
 				return b;
 			}
 			return c;
 		} else {
-			if(a != null) {
+			if (a != null) {
 				return b;
 			}
 			return c;
