4a05bc5142ae89703fafff337a77d6dbb7005d4d
Danilo Pianini <danilo.pianini@unibo.it>
2015-03-03 11:51:05 +0100

    Style fixes

--- src/main/java/it/unibo/alchemist/model/ProtelisIncarnation.java
+++ src/main/java/it/unibo/alchemist/model/ProtelisIncarnation.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2010-2014, Danilo Pianini and contributors
+ * Copyright (C) 2010-2015, Danilo Pianini and contributors
  * listed in the project's pom.xml file.
  * 
  * This file is part of Alchemist, and is distributed under the terms of
@@ -33,14 +33,17 @@ import alice.tuprolog.Var;
  *
  */
 public class ProtelisIncarnation implements Incarnation {
-	
+
 	/*
 	 * search(A, [A|_]).
-search(A, [B|T]) :- search(A, B).
-search(A, [B|T]) :- search(A, T).
-ts(A) :- search(A, [[[['service 1', 1.3]], ["service 2", 0]]]).
+	 * 
+	 * search(A, [B|T]) :- search(A, B).
+	 * 
+	 * search(A, [B|T]) :- search(A, T).
+	 * 
+	 * ts(A) :- search(A, [[[['service 1', 1.3]], ["service 2",0]]]).
 	 */
-	
+
 	private static final Prolog ENGINE = new Prolog();
 	private static final String GOALP1 = "search(";
 	private static final String GOALP2 = ", ";
@@ -63,41 +66,42 @@ ts(A) :- search(A, [[[['service 1', 1.3]], ["service 2", 0]]]).
 		final Object val = node.getConcentration(mol);
 		return getVal(val, prop);
 	}
-	
+
 	private static double getVal(final Object val, final String prop) {
 		if (val instanceof Number) {
 			return ((Number) val).doubleValue();
 		} else if (val instanceof String) {
-			if(val.equals(prop)) {
+			if (val.equals(prop)) {
 				return 1;
 			}
 			return 0;
-		} else if(val instanceof Boolean) {
+		} else if (val instanceof Boolean) {
 			final Boolean cond = (Boolean) val;
-			if(cond) {
+			if (cond) {
 				return 1d;
 			} else {
 				return 0d;
 			}
-		} else if(val instanceof Tuple) {
+		} else if (val instanceof Tuple) {
 			/*
 			 * [[service 1, THIS]*]
 			 * 
-			 * . -> subtuble
-			 * = -> double / string match
-			 * * -> ignore level
+			 * . -> subtuble = -> double / string match * -> ignore level
 			 */
 			final Tuple t = (Tuple) val;
 			try {
 				final SolveInfo s = ENGINE.solve(GOALP1 + prop + GOALP2 + t.toString() + GOALP3);
-				if(s.isSuccess()) {
-					final List<Var> binds =  s.getBindingVars();
-					if(binds.isEmpty()) {
+				if (s.isSuccess()) {
+					@SuppressWarnings("unchecked")
+					final List<Var> binds = s.getBindingVars();
+					if (binds.isEmpty()) {
 						return 1;
 					} else {
-						final Optional<Var> vv = binds.stream().filter(v -> v.getTerm() instanceof alice.tuprolog.Number).findFirst();
-						if(vv.isPresent()) {
-							return ((alice.tuprolog.Number)vv.get().getTerm()).doubleValue();
+						final Optional<Var> vv = binds.stream()
+								.filter(v -> v.getTerm() instanceof alice.tuprolog.Number)
+								.findFirst();
+						if (vv.isPresent()) {
+							return ((alice.tuprolog.Number) vv.get().getTerm()).doubleValue();
 						}
 					}
 				}
@@ -109,14 +113,11 @@ ts(A) :- search(A, [[[['service 1', 1.3]], ["service 2", 0]]]).
 		}
 		return Double.NaN;
 	}
-	
-	
 
 	@Override
 	public IMolecule createMolecule(final String s) {
-		if(s.startsWith(ProtelisProgram.PROGRAM_ID_PREFIX)) {
-			final ProtelisProgram prog = ProtelisProgram.getProgramByID(new FasterString(s));
-			return prog;
+		if (s.startsWith(ProtelisProgram.PROGRAM_ID_PREFIX)) {
+			return ProtelisProgram.getProgramByID(new FasterString(s));
 		}
 		return new Molecule(s);
 	}
