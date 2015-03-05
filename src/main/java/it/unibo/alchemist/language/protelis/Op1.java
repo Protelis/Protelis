1a57bce0fe076ef933cb0e29cde5cd4558f42f93
Danilo Pianini <danilo.pianini@unibo.it>
2015-03-05 18:36:29 +0100

    Style fix

--- src/main/java/it/unibo/alchemist/language/protelis/Op1.java
+++ src/main/java/it/unibo/alchemist/language/protelis/Op1.java
@@ -22,7 +22,13 @@ import java.util.function.UnaryOperator;
  */
 public enum Op1 {
 	
+	/**
+	 * Not.
+	 */
 	NOT("!", Op1::not),
+	/**
+	 * Sign inversion.
+	 */
 	MINUS("-", Op1::minus);
 	
 	private static final int[] FIELDS = new int[]{0};
@@ -35,36 +41,47 @@ public enum Op1 {
 		n = name;
 	}
 	
+	/**
+	 * @param a the object on which the {@link Op1} should be run
+	 * @return the result of the evaluation
+	 */
 	public Object run(final Object a) {
-		if(a instanceof Field) {
+		if (a instanceof Field) {
 			return Field.applyWithSingleParam(fun, FIELDS, a);
 		}
 		return fun.apply(a);
 	}
 	
+	@Override
 	public String toString() {
 		return n;
 	}
 
-	public static Op1 getOp(String name) {
+	/**
+	 * Translates a name into an operator.
+	 * 
+	 * @param name operator name
+	 * @return an {@link Op1}
+	 */
+	public static Op1 getOp(final String name) {
 		Op1 op = MAP.get(name);
-		if(op == null) {
+		if (op == null) {
 			op = Arrays.stream(values()).parallel().filter(o -> o.n.equals(name)).findFirst().get();
 			MAP.put(name, op);
 		}
 		return op;
 	}
 	
-	private static double minus(Object o) {
-		if(o instanceof Number) {
-			return -((Number)o).doubleValue();
+	private static double minus(final Object o) {
+		if (o instanceof Number) {
+			return -((Number) o).doubleValue();
 		}
 		return unsupported("-", o);
 	}
 	
-	private static boolean not(Object o) {
-		if(o instanceof Boolean) {
-			return !(Boolean)o;
+	private static boolean not(final Object o) {
+		if (o instanceof Boolean) {
+			return !(Boolean) o;
 		}
 		return unsupported("!", o);
 	}
