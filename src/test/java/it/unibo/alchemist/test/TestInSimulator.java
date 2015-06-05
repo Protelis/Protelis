9a085136c533ca32bc6846dd05b16d4704c62754
Danilo Pianini <danilo.pianini@unibo.it>
2015-06-05 17:18:53 +0200

    Remove unused lines in simulator test (still is empty)

--- src/test/java/it/unibo/alchemist/test/TestInSimulator.java
+++ src/test/java/it/unibo/alchemist/test/TestInSimulator.java
@@ -7,65 +7,8 @@
  * in the file LICENSE in the Alchemist distribution's top directory.
  */
 package it.unibo.alchemist.test;
-import static org.danilopianini.lang.LangUtils.stackTraceToString;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.fail;
-import gnu.trove.list.array.TByteArrayList;
-import gnu.trove.map.hash.TIntObjectHashMap;
-import it.unibo.alchemist.language.protelis.FunctionDefinition;
-import it.unibo.alchemist.language.protelis.interfaces.AnnotatedTree;
-import it.unibo.alchemist.language.protelis.util.CodePath;
-import it.unibo.alchemist.language.protelis.util.ProtelisLoader;
-import it.unibo.alchemist.language.protelis.util.StackImpl;
-import it.unibo.alchemist.utils.FasterString;
-
-import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
-
-import org.apache.commons.math3.util.Pair;
-import org.danilopianini.io.FileUtilities;
 
 
 public class TestInSimulator {
 
-	private static void testFile(final String file, final Object expectedResult) {
-		testFile(file, 1, expectedResult);
-	}
-	
-	private static void testFile(final String file, final int steps, final Object expectedResult) {
-		final AnnotatedTree<?> program = runProgram(resourceToString(file), steps);
-		assertEquals(expectedResult, program.getAnnotation());
-	}
-	
-	private static void failExeption(final Throwable e) {
-		fail(stackTraceToString(e));
-	}
-	
-	private static String resourceToString(final String uri) {
-		final String path = TestInSimulator.class.getResource(uri).getFile();
-		try {
-			return FileUtilities.fileToString(path);
-		} catch (IOException e) {
-			failExeption(e);
-		}
-		return null;
-	}
-	
-	private static AnnotatedTree<?> runProgram(final String s) {
-		return runProgram(s, 1);
-	}
-	
-	private static AnnotatedTree<?> runProgram(final String s, final int runs) {
-		final Pair<AnnotatedTree<?>, Map<FasterString, FunctionDefinition>> prog = ProtelisLoader.parse(null, null, null, null, s);
-		AnnotatedTree<?> program = prog.getFirst();
-		Map<CodePath, Object> lastExec = new HashMap<>();
-		for (int i = 0; i < runs; i++) {
-			program = program.copy();
-			final Map<CodePath, Object> newExec = new HashMap<>();
-			program.eval(null, new TIntObjectHashMap<>(), new StackImpl(new HashMap<>(prog.getSecond())), lastExec, newExec, new TByteArrayList());
-			lastExec = newExec;
-		}
-		return program;
-	}
 }
