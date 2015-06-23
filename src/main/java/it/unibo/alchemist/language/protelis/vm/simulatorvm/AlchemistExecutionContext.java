8cc433d777b6eaa89b7f62cf430fb0d19ad1cd62
Danilo Pianini <danilo.pianini@unibo.it>
2015-06-23 10:45:54 +0200

    Add routing support, add dependency on maps

--- src/main/java/it/unibo/alchemist/language/protelis/vm/simulatorvm/AlchemistExecutionContext.java
+++ src/main/java/it/unibo/alchemist/language/protelis/vm/simulatorvm/AlchemistExecutionContext.java
@@ -4,12 +4,15 @@
 package it.unibo.alchemist.language.protelis.vm.simulatorvm;
 
 import it.unibo.alchemist.external.cern.jet.random.engine.RandomEngine;
+import it.unibo.alchemist.language.protelis.datatype.Tuple;
 import it.unibo.alchemist.language.protelis.util.DeviceUID;
 import it.unibo.alchemist.language.protelis.util.DeviceUIDImpl;
 import it.unibo.alchemist.language.protelis.vm.AbstractExecutionContext;
 import it.unibo.alchemist.model.implementations.molecules.Molecule;
 import it.unibo.alchemist.model.implementations.nodes.ProtelisNode;
+import it.unibo.alchemist.model.implementations.positions.LatLongPosition;
 import it.unibo.alchemist.model.interfaces.IEnvironment;
+import it.unibo.alchemist.model.interfaces.IMapEnvironment;
 import it.unibo.alchemist.model.interfaces.IMolecule;
 import it.unibo.alchemist.model.interfaces.INode;
 import it.unibo.alchemist.model.interfaces.IPosition;
@@ -93,6 +96,15 @@ public class AlchemistExecutionContext extends AbstractExecutionContext {
 		newEnvironment.entrySet().forEach(e -> {
 			node.setConcentration(new Molecule(e.getKey()), e.getValue());
 		});
+		if (newEnvironment.size() != node.getChemicalSpecies()) {
+			/*
+			 * There has been a removal
+			 */
+			node.getContents().keySet().stream().parallel()
+				.filter(mol -> mol instanceof Molecule)
+				.filter(mol -> !newEnvironment.containsKey(((Molecule) mol).toFasterString()))
+				.forEach(mol -> node.removeConcentration(mol));
+		}
 	}
 
 	@Override
@@ -103,6 +115,32 @@ public class AlchemistExecutionContext extends AbstractExecutionContext {
 	@Override
 	protected DeviceUID deviceFromId(final long id) {
 		return new DeviceUIDImpl(id);
+	}
+	
+	public double routingDistance(final DeviceUID dest) {
+		return routingDistance(dest.getId());
+	}
+	
+	public double routingDistance(final Tuple dest) {
+		if (dest.size() == 2) {
+			return routingDistance(new LatLongPosition((Number) dest.get(0), (Number) dest.get(1)));
+		}
+		throw new IllegalArgumentException(dest + " is not a coordinate I can understand.");
+	}
+	
+	public double routingDistance(final Number dest) {
+		return routingDistance(env.getNodeByID(dest.intValue()));
+	}
+	
+	public double routingDistance(final INode<Object> dest) {
+		return routingDistance(env.getPosition(dest));
+	}
+	
+	public double routingDistance(final IPosition dest) {
+		if (env instanceof IMapEnvironment<?>) {
+			return ((IMapEnvironment<Object>) env).computeRoute(node, dest).getDistance();
+		}
+		return getDevicePosition().getDistanceTo(dest);
 	}
 	
 }
