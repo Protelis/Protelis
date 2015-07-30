c09ad003f3e9df06a06f836ca91a04511755d4e1
Danilo Pianini <danilo.pianini@unibo.it>
2015-07-30 12:51:37 +0200

    Force update from old header to new. Solves https://github.com/Protelis/Protelis/issues/10

--- src/main/java/org/protelis/lang/datatype/impl/AbstractField.java
+++ src/main/java/org/protelis/lang/datatype/impl/AbstractField.java
@@ -1,11 +1,11 @@
-/*
- * Copyright (C) 2010-2015, Danilo Pianini and contributors
- * listed in the project's pom.xml file.
- * 
- * This file is part of Alchemist, and is distributed under the terms of
+/*******************************************************************************
+ * Copyright (C) 2014, 2015, Danilo Pianini and contributors
+ * listed in the project's build.gradle or pom.xml file.
+ *
+ * This file is part of Protelis, and is distributed under the terms of
  * the GNU General Public License, with a linking exception, as described
- * in the file LICENSE in the Alchemist distribution's top directory.
- */
+ * in the file LICENSE.txt in this project's top directory.
+ *******************************************************************************/
 package org.protelis.lang.datatype.impl;
 
 import java.util.Objects;
