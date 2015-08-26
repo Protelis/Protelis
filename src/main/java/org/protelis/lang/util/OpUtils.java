23c9f32548888658cecd57962fb6ba0ea83a0ac8
jakebeal <jakebeal@alum.mit.edu>
2015-08-25 22:38:38 -0500

    remove requirement for author in checkstyle

--- src/main/java/org/protelis/lang/util/OpUtils.java
+++ src/main/java/org/protelis/lang/util/OpUtils.java
@@ -9,8 +9,7 @@
 package org.protelis.lang.util;
 
 /**
- * @author Danilo Pianini
- * Utility class that OpN classes use for reporting certain failure patterns
+ * Utility class that OpN classes use for reporting certain failure patterns.
  */
 public final class OpUtils {
 
