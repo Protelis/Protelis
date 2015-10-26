6a0ac78783dbd36397f80dc3c9c016c0109542fc
Danilo Pianini <danilo.pianini@unibo.it>
2015-10-26 17:28:16 +0100

    Style fix on OpUtils

--- src/main/java/org/protelis/lang/util/OpUtils.java
+++ src/main/java/org/protelis/lang/util/OpUtils.java
@@ -12,47 +12,42 @@ package org.protelis.lang.util;
  * Utility class that OpN classes use for reporting certain failure patterns.
  */
 public final class OpUtils {
-	
-	/*
-	 * TODO: probably it is more java-like to create a custom exception
-	 * extending UnsupportedOperationException.
-	 */
-	// Not constructable: can only be used for static methods
-	private OpUtils() {
-	}
-	
-	/**
-	 * This method builds a meaningful exception and throws it immediately.
-	 * 
-	 * @param op
-	 *            The name of the operation
-	 * @param a
-	 *            The arguments that have been passed, and that made the
-	 *            operation fail.
-	 * @param <T>
-	 *            Return type. This function never returns, this type is a
-	 *            placeholder that allows you to write stuff like
-	 *            "return unsupported(op, args)".
-	 * @throws UnsupportedOperationException
-	 *             ALWAYS
-	 * @return Nothing, since it throws an exception
-	 */
-	public static <T> T unsupported(final String op, final Object... a) {
-		final StringBuilder msg = new StringBuilder("Nobody told me how to run ");
-		msg.append(op);
-		if (a.length > 0) {
-			msg.append(" with parameters of class: ");
-			boolean first = true;
-			for (final Object o : a) {
-				if (first) { 
-					first = false; 
-				} else { 
-					msg.append(", "); 
-				}
-				msg.append(o == null ? "null" : o.getClass().getSimpleName());
-			}
-			msg.append('.');
-		}
-		throw new UnsupportedOperationException(msg.toString());
-	}	
+
+    private OpUtils() {
+    }
+
+    /**
+     * This method builds a meaningful exception and throws it immediately.
+     * 
+     * @param op
+     *            The name of the operation
+     * @param a
+     *            The arguments that have been passed, and that made the
+     *            operation fail.
+     * @param <T>
+     *            Return type. This function never returns, this type is a
+     *            placeholder that allows you to write stuff like
+     *            "return unsupported(op, args)".
+     * @throws UnsupportedOperationException
+     *             ALWAYS
+     * @return Nothing, since it throws an exception
+     */
+    public static <T> T unsupported(final String op, final Object... a) {
+        final StringBuilder msg = new StringBuilder("Nobody told me how to run ");
+        msg.append(op);
+        if (a.length > 0) {
+            msg.append(" with parameters of class: ");
+            boolean first = true;
+            for (final Object o : a) {
+                if (first) {
+                    first = false;
+                } else {
+                    msg.append(", ");
+                }
+                msg.append(o == null ? "null" : o.getClass().getSimpleName());
+            }
+            msg.append('.');
+        }
+        throw new UnsupportedOperationException(msg.toString());
+    }
 }
