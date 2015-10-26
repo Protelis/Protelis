b8d941154e22c1f08f9a3b5188a00efb67a8edd9
Danilo Pianini <danilo.pianini@unibo.it>
2015-10-26 17:22:39 +0100

    Style fix on Op1

--- src/main/java/org/protelis/lang/util/Op1.java
+++ src/main/java/org/protelis/lang/util/Op1.java
@@ -18,71 +18,73 @@ import java.util.function.UnaryOperator;
 import org.protelis.lang.datatype.Field;
 
 /**
- *	Collection of functions and helper methods for unary operators.
+ * Collection of functions and helper methods for unary operators.
  */
 public enum Op1 {
-	
-	/**
-	 * Not.
-	 */
-	NOT("!", Op1::not),
-	/**
-	 * Sign inversion.
-	 */
-	MINUS("-", Op1::minus);
-	
-	private static final int[] FIELDS = new int[]{0};
-	private static final Map<String, Op1> MAP = new ConcurrentHashMap<>();
-	private final UnaryOperator<Object> fun;
-	private final String opName;
-	
-	Op1(final String name, final UnaryOperator<Object> function) {
-		fun = function;
-		opName = name;
-	}
-	
-	/**
-	 * @param a the object on which the {@link Op1} should be run
-	 * @return the result of the evaluation
-	 */
-	public Object run(final Object a) {
-		if (a instanceof Field) {
-			return Field.applyWithSingleParam(fun, FIELDS, a);
-		}
-		return fun.apply(a);
-	}
-	
-	@Override
-	public String toString() {
-		return opName;
-	}
 
-	/**
-	 * Translates a name into an operator.
-	 * 
-	 * @param name operator name
-	 * @return an {@link Op1}
-	 */
-	public static Op1 getOp(final String name) {
-		Op1 op = MAP.get(name);
-		if (op == null) {
-			op = Arrays.stream(values()).parallel().filter(o -> o.opName.equals(name)).findFirst().get();
-			MAP.put(name, op);
-		}
-		return op;
-	}
-	
-	private static double minus(final Object o) {
-		if (o instanceof Number) {
-			return -((Number) o).doubleValue();
-		}
-		return unsupported("-", o);
-	}
-	
-	private static boolean not(final Object o) {
-		if (o instanceof Boolean) {
-			return !(Boolean) o;
-		}
-		return unsupported("!", o);
-	}
+    /**
+     * Not.
+     */
+    NOT("!", Op1::not),
+    /**
+     * Sign inversion.
+     */
+    MINUS("-", Op1::minus);
+
+    private static final int[] FIELDS = new int[] { 0 };
+    private static final Map<String, Op1> MAP = new ConcurrentHashMap<>();
+    private final UnaryOperator<Object> fun;
+    private final String opName;
+
+    Op1(final String name, final UnaryOperator<Object> function) {
+        fun = function;
+        opName = name;
+    }
+
+    /**
+     * @param a
+     *            the object on which the {@link Op1} should be run
+     * @return the result of the evaluation
+     */
+    public Object run(final Object a) {
+        if (a instanceof Field) {
+            return Field.applyWithSingleParam(fun, FIELDS, a);
+        }
+        return fun.apply(a);
+    }
+
+    @Override
+    public String toString() {
+        return opName;
+    }
+
+    /**
+     * Translates a name into an operator.
+     * 
+     * @param name
+     *            operator name
+     * @return an {@link Op1}
+     */
+    public static Op1 getOp(final String name) {
+        Op1 op = MAP.get(name);
+        if (op == null) {
+            op = Arrays.stream(values()).parallel().filter(o -> o.opName.equals(name)).findFirst().get();
+            MAP.put(name, op);
+        }
+        return op;
+    }
+
+    private static double minus(final Object o) {
+        if (o instanceof Number) {
+            return -((Number) o).doubleValue();
+        }
+        return unsupported("-", o);
+    }
+
+    private static boolean not(final Object o) {
+        if (o instanceof Boolean) {
+            return !(Boolean) o;
+        }
+        return unsupported("!", o);
+    }
 }
