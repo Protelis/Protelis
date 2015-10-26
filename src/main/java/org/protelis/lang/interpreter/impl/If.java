86258bd27f6baef98e370d08ba0f38dbe9f13fc9
Danilo Pianini <danilo.pianini@unibo.it>
2015-10-26 17:09:46 +0100

    Style fix on If

--- src/main/java/org/protelis/lang/interpreter/impl/If.java
+++ src/main/java/org/protelis/lang/interpreter/impl/If.java
@@ -13,68 +13,78 @@ import org.protelis.lang.interpreter.AnnotatedTree;
 import org.protelis.vm.ExecutionContext;
 
 /**
- * Branch, restricting domain of true and false branches into their own aligned subspaces.
+ * Branch, restricting domain of true and false branches into their own aligned
+ * subspaces.
  *
  * @param <T>
  */
 public class If<T> extends AbstractAnnotatedTree<T> {
 
-	private static final long serialVersionUID = -4830593657731078743L;
-	private static final byte COND = 0, THEN = 1, ELSE = 2;
-	private final AnnotatedTree<Boolean> conditionExpression;
-	private final AnnotatedTree<T> thenExpression, elseExpression;
+    private static final long serialVersionUID = -4830593657731078743L;
+    private static final byte COND = 0, THEN = 1, ELSE = 2;
+    private final AnnotatedTree<Boolean> conditionExpression;
+    private final AnnotatedTree<T> thenExpression, elseExpression;
 
-	/**
-	 * @param cond
-	 *            condition
-	 * @param then
-	 *            branch to execute if condition is true (erase otherwise)
-	 * @param otherwise
-	 *            branch to execute if condition is false (erase otherwise)
-	 */
-	public If(final AnnotatedTree<Boolean> cond, final AnnotatedTree<T> then, final AnnotatedTree<T> otherwise) {
-		super(cond, then, otherwise);
-		conditionExpression = cond;
-		thenExpression = then;
-		elseExpression = otherwise;
-	}
+    /**
+     * @param cond
+     *            condition
+     * @param then
+     *            branch to execute if condition is true (erase otherwise)
+     * @param otherwise
+     *            branch to execute if condition is false (erase otherwise)
+     */
+    public If(final AnnotatedTree<Boolean> cond, final AnnotatedTree<T> then, final AnnotatedTree<T> otherwise) {
+        super(cond, then, otherwise);
+        conditionExpression = cond;
+        thenExpression = then;
+        elseExpression = otherwise;
+    }
 
-	@Override
-	public AnnotatedTree<T> copy() {
-		return new If<>(conditionExpression.copy(), thenExpression.copy(), elseExpression.copy());
-	}
+    @Override
+    public AnnotatedTree<T> copy() {
+        return new If<>(conditionExpression.copy(), thenExpression.copy(), elseExpression.copy());
+    }
 
-	@Override
-	public void eval(final ExecutionContext context) {
-		conditionExpression.evalInNewStackFrame(context, COND);
-		final Object actualResult = conditionExpression.getAnnotation();
-		final boolean bool = actualResult instanceof Boolean ? conditionExpression.getAnnotation() : actualResult != null;
-		setAnnotation(bool ? choice(THEN, thenExpression, elseExpression, context) : choice(ELSE, elseExpression, thenExpression, context));
-	}
+    @Override
+    public void eval(final ExecutionContext context) {
+        conditionExpression.evalInNewStackFrame(context, COND);
+        final Object actualResult = conditionExpression.getAnnotation();
+        final boolean bool = actualResult instanceof Boolean
+                ? conditionExpression.getAnnotation()
+                : actualResult != null;
+        setAnnotation(bool
+                ? choice(THEN, thenExpression, elseExpression, context)
+                : choice(ELSE, elseExpression, thenExpression, context));
+    }
 
-	private static <T> T choice(final byte branch, final AnnotatedTree<T> selected, final AnnotatedTree<T> erased, final ExecutionContext context) {
-		selected.evalInNewStackFrame(context, branch);
-		erased.erase();
-		final T result = selected.getAnnotation();
-		if (result instanceof Field) {
-			throw new IllegalStateException("if statements cannot return a Field. This could break apart alignment. Consider using mux.");
-		}
-		return result;
-	}
+    private static <T> T choice(
+            final byte branch,
+            final AnnotatedTree<T> selected,
+            final AnnotatedTree<T> erased,
+            final ExecutionContext context) {
+        selected.evalInNewStackFrame(context, branch);
+        erased.erase();
+        final T result = selected.getAnnotation();
+        if (result instanceof Field) {
+            throw new IllegalStateException(
+                    "if statements cannot return a Field. This could break alignment apart. Consider using mux.");
+        }
+        return result;
+    }
 
-	@Override
-	protected void asString(final StringBuilder sb, final int i) {
-		sb.append("if (\n");
-		conditionExpression.toString(sb, i + 1);
-		sb.append(") {\n");
-		thenExpression.toString(sb, i + 1);
-		sb.append('\n');
-		indent(sb, i);
-		sb.append("} else {\n");
-		elseExpression.toString(sb, i + 1);
-		sb.append('\n');
-		indent(sb, i);
-		sb.append('}');
-	}
+    @Override
+    protected void asString(final StringBuilder sb, final int i) {
+        sb.append("if (\n");
+        conditionExpression.toString(sb, i + 1);
+        sb.append(") {\n");
+        thenExpression.toString(sb, i + 1);
+        sb.append('\n');
+        indent(sb, i);
+        sb.append("} else {\n");
+        elseExpression.toString(sb, i + 1);
+        sb.append('\n');
+        indent(sb, i);
+        sb.append('}');
+    }
 
 }
