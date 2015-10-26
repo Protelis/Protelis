63046582b4e835534d4ccb829a06b37c208d1f68
Danilo Pianini <danilo.pianini@unibo.it>
2015-10-26 17:15:51 +0100

    Style fix on UnaryOp

--- src/main/java/org/protelis/lang/interpreter/impl/UnaryOp.java
+++ src/main/java/org/protelis/lang/interpreter/impl/UnaryOp.java
@@ -15,45 +15,45 @@ import org.protelis.lang.util.Op1;
 import org.protelis.vm.ExecutionContext;
 
 /**
- *	Unary (prefix) operator, such as negation.
+ * Unary (prefix) operator, such as negation.
  *
  */
 public class UnaryOp extends AbstractAnnotatedTree<Object> {
 
-	private static final long serialVersionUID = 2803028109250981637L;
-	private final Op1 op;
-	
-	/**
-	 * @param name
-	 *            operator name
-	 * @param branch
-	 *            the operand
-	 */
-	public UnaryOp(final String name, final AnnotatedTree<?> branch) {
-		this(Op1.getOp(name), branch);
-	}
-	
-	private UnaryOp(final Op1 operator, final AnnotatedTree<?> branch) {
-		super(branch);
-		Objects.requireNonNull(branch);
-		op = operator;
-	}
+    private static final long serialVersionUID = 2803028109250981637L;
+    private final Op1 op;
 
-	@Override
-	public UnaryOp copy() {
-		return new UnaryOp(op, getBranch(0).copy());
-	}
+    /**
+     * @param name
+     *            operator name
+     * @param branch
+     *            the operand
+     */
+    public UnaryOp(final String name, final AnnotatedTree<?> branch) {
+        this(Op1.getOp(name), branch);
+    }
 
-	@Override
-	public void eval(final ExecutionContext context) {
-		projectAndEval(context);
-		setAnnotation(op.run(getBranch(0).getAnnotation()));
-	}
+    private UnaryOp(final Op1 operator, final AnnotatedTree<?> branch) {
+        super(branch);
+        Objects.requireNonNull(branch);
+        op = operator;
+    }
 
-	@Override
-	protected void asString(final StringBuilder sb, final int i) {
-		sb.append(op);
-		getBranch(0).toString(sb, i);
-	}
+    @Override
+    public UnaryOp copy() {
+        return new UnaryOp(op, getBranch(0).copy());
+    }
+
+    @Override
+    public void eval(final ExecutionContext context) {
+        projectAndEval(context);
+        setAnnotation(op.run(getBranch(0).getAnnotation()));
+    }
+
+    @Override
+    protected void asString(final StringBuilder sb, final int i) {
+        sb.append(op);
+        getBranch(0).toString(sb, i);
+    }
 
 }
