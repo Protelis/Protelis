cb55121b55dd4126027a130ef1dbcd388b2118ef
mfrancia <matteo.francia2@studio.unibo.it>
2016-10-08 02:26:19 +0200

    spreading tests are ok

--- protelis/protelis-lang/src/test/java/Results.java
+++ protelis/protelis-lang/src/test/java/Results.java
@@ -1,4 +1,4 @@
-package org.protelis.test;
+import org.protelis.test.IntegerUID;
 
 /**
  * Well known results of the tests.
@@ -15,16 +15,49 @@ public final class Results {
     /**
      * 4x3 squared matrix made of FOO values.
      */
-    public static final Object[][] BROADCAST = { { FOO, FOO, FOO }, { FOO, FOO, FOO }, { FOO, FOO, FOO },
-                    { FOO, FOO, FOO } };
+    public static final Object[][] BROADCAST = { 
+                    { FOO, FOO, FOO }, 
+                    { FOO, FOO, FOO }, 
+                    { FOO, FOO, FOO },
+                    { FOO, FOO, FOO } 
+    };
 
     /**
-     * nbrRange result.
+     * nbrRange.pt result.
      */
     public static final Object[] NBRRANGE = { 1.0, 1.0 };
 
     /**
-     * neighborhood result.
+     * neighborhood.pt result.
      */
-    public static final Object[][] NEIGHBORHOOD = { { 2.0, 3.0, 2.0 }, { 3.0, 4.0, 3.0 }, { 2.0, 3.0, 2.0 } };
+    public static final Object[][] NEIGHBORHOOD = { 
+                    { 2.0, 3.0, 2.0 }, 
+                    { 3.0, 4.0, 3.0 }, 
+                    { 2.0, 3.0, 2.0 } 
+    };
+
+    private static final String S0 = new IntegerUID(0).toString(), 
+                    S3 = new IntegerUID(3).toString(), 
+                    S12 = new IntegerUID(12).toString(), 
+                    S15 = new IntegerUID(15).toString();
+
+    /**
+     * G.pt result.
+     */
+    public static final Object[][] G = {
+                    { S0, S0, S3, S3 },
+                    { S0, S0, S3, S3 },
+                    { S12, S12, S15, S15 },
+                    { S12, S12, S15, S15 }
+    };
+
+    /**
+     * distance.pt result.
+     */
+    public static final Object[][] DISTANCE = {
+                    { 5.0, 5.0, 5.0 },
+                    { 5.0, 5.0, 5.0 },
+                    { 5.0, 5.0, 5.0 },
+                    { 5.0, 5.0, 5.0 }
+    };
 }
--- protelis/protelis-lang/src/test/java/TestApis.java
+++ protelis/protelis-lang/src/test/java/TestApis.java
@@ -1,7 +1,6 @@
 import static org.junit.Assert.assertArrayEquals;
 
 import org.junit.Test;
-import org.protelis.test.Results;
 import org.protelis.test.SimulationTest;
 
 /**
@@ -47,6 +46,30 @@ public class TestApis {
     public void testNbrRange() {
         testFileWithExplicitResult(new SimulationTest("nbrRange", MAX_CYCLE_NUM, 2, 1, 1).getResults(),
                         Results.NBRRANGE);
+    }
+
+    /**
+     * Test G.pt.
+     */
+    @Test
+    public void testG() {
+        SimulationTest sim = new SimulationTest("G", MAX_CYCLE_NUM, 4, 4, 1);
+        sim.setProperty(0, "source", true);
+        sim.setProperty(3, "source", true);
+        sim.setProperty(12, "source", true);
+        sim.setProperty(15, "source", true);
+        testFileWithExplicitResult(sim.getResults(), Results.G);
+    }
+
+    /**
+     * Test G.pt.
+     */
+    @Test
+    public void testDistance() {
+        SimulationTest sim = new SimulationTest("distance", MAX_CYCLE_NUM, 3, 4, 1);
+        sim.setProperty(0, "source", true);
+        sim.setProperty(11, "destination", true);
+        testFileWithExplicitResult(sim.getResults(), Results.DISTANCE);
     }
 
     /*
