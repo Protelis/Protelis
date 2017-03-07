2ab0856c91f4b1880acf23b091bdae745595ac4c
jakebeal <jakebeal@alum.mit.edu>
2017-03-07 14:00:02 -0600

    add "toArray" method to tuples

--- protelis/protelis-interpreter/src/main/java/org/protelis/lang/datatype/Tuple.java
+++ protelis/protelis-interpreter/src/main/java/org/protelis/lang/datatype/Tuple.java
@@ -281,5 +281,9 @@ public interface Tuple extends Iterable<Object>, Serializable, Comparable<Tuple>
      *         otherwise
      */
     Object max(Object def);
-
+    
+    /**
+     * @return an array with a copy of the contents of the tuple
+     */
+    Object[] toArray();
 }
