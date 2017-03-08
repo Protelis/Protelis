8a8071e94f3192f8af2dca6b00e72785f24597b2
Danilo Pianini <danilo.pianini@unibo.it>
2017-03-08 14:56:52 +0100

    Bring scripts on the outer level

--- before_install.sh
+++ before_install.sh
@@ -1,2 +1,3 @@
+#!/bin/bash
 openssl aes-256-cbc -K $encrypted_e9b2eb0f7f1a_key -iv $encrypted_e9b2eb0f7f1a_iv -in prepare_environment.sh.enc -out prepare_environment.sh -d
 sh prepare_environment.sh
