166d83187481fb62e59f6d6fd1223009a8c1cfd7
Danilo Pianini <danilo.pianini@unibo.it>
2018-11-05 17:10:13 +0100

    Fix unused imports and javadoc reference

--- protelis/protelis-interpreter/src/main/java/org/protelis/lang/datatype/impl/LongUID.java
+++ protelis/protelis-interpreter/src/main/java/org/protelis/lang/datatype/impl/LongUID.java
@@ -1,13 +1,11 @@
 package org.protelis.lang.datatype.impl;
 
-import org.protelis.lang.datatype.DeviceUID;
-
 /** Simple long UIDs. */
 public class LongUID extends AbstractComparableDeviceUID<Long> {
     private static final long serialVersionUID = 1L;
 
     /**
-     * Create {@link DeviceUID} from a long.
+     * Create {@link org.protelis.lang.datatype.DeviceUID} from a long.
      * 
      * @param uid
      *            the value
--- protelis/protelis-interpreter/src/main/java/org/protelis/lang/datatype/impl/StringUID.java
+++ protelis/protelis-interpreter/src/main/java/org/protelis/lang/datatype/impl/StringUID.java
@@ -1,13 +1,11 @@
 package org.protelis.lang.datatype.impl;
 
-import org.protelis.lang.datatype.DeviceUID;
-
 /** Simple string UIDs. */
 public class StringUID extends AbstractComparableDeviceUID<String> {
     private static final long serialVersionUID = 1L;
 
     /**
-     * Create {@link DeviceUID} from a string.
+     * Create {@link org.protelis.lang.datatype.DeviceUID} from a string.
      * 
      * @param uid
      *            the value
