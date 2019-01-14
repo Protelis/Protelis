7cb382c9440618e1e7ab2b96bbaedec7200e1525
Danilo Pianini <danilo.pianini@unibo.it>
2019-01-14 18:08:43 +0100

    Enable build scans

--- travis_build.sh
+++ travis_build.sh
@@ -3,6 +3,6 @@ set -e
 openssl aes-256-cbc -K $encrypted_e9b2eb0f7f1a_key -iv $encrypted_e9b2eb0f7f1a_iv -in prepare_environment.sh.enc -out prepare_environment.sh -d
 sh prepare_environment.sh
 cd protelis
-./gradlew clean check fatJar projectReport publish
+./gradlew clean check fatJar projectReport publish --scan
 mkdir -p report
 cp --parent */build/reports build/reports report -R
