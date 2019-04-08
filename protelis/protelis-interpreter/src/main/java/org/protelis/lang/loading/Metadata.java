3b46865268f6541eeccb666d5a2071ef186516b0
Danilo Pianini <danilo.pianini@unibo.it>
2019-04-08 18:30:28 +0200

    Document metadata

--- protelis/protelis-interpreter/src/main/java/org/protelis/lang/loading/Metadata.java
+++ protelis/protelis-interpreter/src/main/java/org/protelis/lang/loading/Metadata.java
@@ -2,10 +2,20 @@ package org.protelis.lang.loading;
 
 import java.io.Serializable;
 
+/**
+ * This class represents data about each AST node about the original code that generated it.
+ *
+ */
 public interface Metadata extends Serializable {
 
+    /**
+     * @return the line number at which the entity is defined
+     */
     int getStartLine();
 
+    /**
+     * @return the line number at which the entity terminates
+     */
     int getEndLine();
 
 }
