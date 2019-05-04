b01603a9e51f109133ad972c8ec5c52880e03a8c
Danilo Pianini <danilo.pianini@gmail.com>
2019-05-04 16:58:40 +0200

    Handle JVM references uniformly

--- protelis/protelis-interpreter/src/main/java/org/protelis/lang/interpreter/util/Reference.java
+++ protelis/protelis-interpreter/src/main/java/org/protelis/lang/interpreter/util/Reference.java
@@ -3,24 +3,25 @@ package org.protelis.lang.interpreter.util;
 import java.io.Serializable;
 import java.lang.reflect.InvocationTargetException;
 import java.util.Objects;
-import java8.util.Optional;
 
 import org.danilopianini.lang.HashUtils;
 import org.eclipse.emf.ecore.EObject;
-import org.eclipse.xtext.common.types.JvmOperation;
+import org.eclipse.xtext.common.types.JvmIdentifiableElement;
 import org.protelis.parser.protelis.FunctionDef;
 import org.protelis.parser.protelis.ProtelisModule;
 import org.protelis.parser.protelis.VarDef;
 
+import java8.util.Optional;
+
 /**
- * Implements a serializable reference to an Object. This implementation is
+ * Implements a Serializable reference to an Object. This implementation is
  * fragile, and should be substituted as soon as the Java board decides what to
  * do with sun.misc.Unsafe (that might get dropped).
  */
 public final class Reference implements Serializable {
 
     private static final long serialVersionUID = 8294777860793746504L;
-    private final long uid;
+    private final Serializable uid;
     private String strRep;
 
     /**
@@ -40,36 +41,37 @@ public final class Reference implements Serializable {
             }
             uid = hash;
             strRep = var.getName();
-        } else if (obj instanceof JvmOperation) {
-            final JvmOperation method = (JvmOperation) obj;
+        } else if (obj instanceof JvmIdentifiableElement) {
+            final JvmIdentifiableElement method = (JvmIdentifiableElement) obj;
             strRep = method.getQualifiedName();
-            uid = HashUtils.hash64(strRep);
+            uid = strRep;
         } else if (obj instanceof FunctionDef) {
             final FunctionDef function = (FunctionDef) obj;
             final ProtelisModule container = (ProtelisModule) function.eContainer();
             final String name = Optional.ofNullable(container.getName()).orElse("default-module") + ":";
             strRep = name + function.getName();
-            uid = HashUtils.hash64(strRep);
+            uid = strRep;
+        } else if (obj instanceof Serializable) {
+            uid = (Serializable) Objects.requireNonNull(obj);
         } else {
-            Objects.requireNonNull(obj);
-            uid = System.identityHashCode(obj);
+            throw new IllegalArgumentException("Cannot refer to " + obj);
         }
     }
 
     @Override
     public int hashCode() {
-        return (int) uid;
+        return uid.hashCode();
     }
 
     @Override
     public boolean equals(final Object obj) {
-        return obj instanceof Reference && ((Reference) obj).uid == uid;
+        return obj instanceof Reference && ((Reference) obj).uid.equals(uid);
     }
 
     @Override
     public String toString() {
         if (strRep == null) {
-            strRep = "Var@" + Long.toString(uid, Character.MAX_RADIX);
+            strRep = "Var@" + uid;
         }
         return strRep;
     }
