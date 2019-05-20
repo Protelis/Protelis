95a537bf5481994ee24c77d6191cfafbda31e406
Danilo Pianini <danilo.pianini@unibo.it>
2019-05-20 17:01:53 +0200

    Exclude poisonous dependency (asm-debug-all)

--- protelis/protelis-test/build.gradle.kts
+++ protelis/protelis-test/build.gradle.kts
@@ -1,7 +1,11 @@
 dependencies {
     api(project(":protelis-interpreter"))
-    implementation("it.unibo.alchemist:alchemist-interfaces:${extra["alchemistVersion"]}")
-    implementation("it.unibo.alchemist:alchemist-loading:${extra["alchemistVersion"]}")
+    implementation("it.unibo.alchemist:alchemist-interfaces:${extra["alchemistVersion"]}") {
+        exclude(module = "asm-debug-all")
+    }
+    implementation("it.unibo.alchemist:alchemist-loading:${extra["alchemistVersion"]}") {
+        exclude(module = "asm-debug-all")
+    }
     implementation("junit:junit:${extra["junitVersion"]}")
     implementation("org.apache.commons:commons-lang3:${extra["lang3Version"]}")
 }
