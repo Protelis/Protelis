9e9d1c010dd728b5ed187f87726987f42d6e1a1d
Roberto Casadei <robyonrails@gmail.com>
2019-06-06 10:18:17 +0200

    Fix Protelis2Doc plugin (version without automatic depencency resolution)  application to protelis-lang

--- protelis/protelis-lang/build.gradle.kts
+++ protelis/protelis-lang/build.gradle.kts
@@ -1,6 +1,11 @@
+plugins {
+    id("org.protelis.protelisdoc") version "0.1.1-dev0f+1c1a147"
+}
+
 dependencies {
     implementation(project(":protelis-interpreter"))
     testImplementation(project(":protelis-test"))
+    implementation(kotlin("stdlib"))
 }
 
 sourceSets {
