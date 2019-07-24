498ca6adba07fda16858a847339b5fd9a9d8edf5
Danilo Pianini <danilo.pianini@unibo.it>
2019-07-24 14:39:40 +0200

    Temporarily remove protelisdoc, as raises issues with ktlint

--- protelis/protelis-lang/build.gradle.kts
+++ protelis/protelis-lang/build.gradle.kts
@@ -1,7 +1,7 @@
 import org.apache.tools.ant.taskdefs.condition.Os
 
 plugins {
-    id("org.protelis.protelisdoc") version Versions.org_protelis_protelisdoc_gradle_plugin
+//    id("org.protelis.protelisdoc") version Versions.org_protelis_protelisdoc_gradle_plugin
 }
 
 dependencies {
