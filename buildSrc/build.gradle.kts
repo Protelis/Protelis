5fd4996b7c832b2c7881555320a429fe52a90785
Danilo Pianini <danilo.pianini@gmail.com>
2021-07-22 17:22:51 +0000

    [Autodelivery] update buildSrc infrastructure from DanySK/centralized-automated-deployer@c5928ae

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -1,15 +1,29 @@
-/*
- * Copyright (C) 2021, Danilo Pianini and contributors listed in the project's build.gradle.kts or pom.xml file.
- *
- * This file is part of Protelis, and is distributed under the terms of the GNU General Public License,
- * with a linking exception, as described in the file LICENSE.txt in this project's top directory.
- */
-
-
 plugins {
     `kotlin-dsl`
+    id("io.gitlab.arturbosch.detekt")
+    id("org.jlleitschuh.gradle.ktlint")
 }
+
 repositories {
-    jcenter()
+    mavenCentral()
+    gradlePluginPortal()
 }
-        
+
+dependencies {
+    detektPlugins("io.gitlab.arturbosch.detekt:detekt-formatting:_")
+}
+
+detekt {
+    allRules = true
+    buildUponDefaultConfig = true
+    config = files("$rootDir/../config/detekt/detekt.yml")
+}
+
+projectDir.listFiles()
+    ?.filter { it.isFile && it.name.endsWith("dependencies") }
+    ?.flatMap { it.readLines() }
+    ?.forEach {
+        dependencies {
+            implementation(it)
+        }
+    }
