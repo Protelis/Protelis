896135555f88c580a2f1fbb04bd7950c871aa0fd
Danilo Pianini <danilo.pianini@gmail.com>
2022-03-06 00:05:10 +0100

    docs(interpreter): document HashingFunnel

--- protelis-interpreter/src/main/java/org/protelis/lang/interpreter/util/HashingFunnel.java
+++ protelis-interpreter/src/main/java/org/protelis/lang/interpreter/util/HashingFunnel.java
@@ -5,6 +5,9 @@ import org.protelis.lang.datatype.Either;
 import java.io.Serializable;
 import java.util.function.Function;
 
+/**
+ * Serializable function that can return either an {@link Integer} or a byte[].
+ */
 @FunctionalInterface
 public interface HashingFunnel extends Function<Object, Either<Integer, byte[]>>, Serializable {
 }
