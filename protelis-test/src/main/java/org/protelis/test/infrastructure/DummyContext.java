278e29ef512bd4da52f39425e0c77cd33eb774da
Danilo Pianini <danilo.pianini@unibo.it>
2023-02-22 12:53:15 +0100

    style(test): remove unnecessary semicolon

--- protelis-test/src/main/java/org/protelis/test/infrastructure/DummyContext.java
+++ protelis-test/src/main/java/org/protelis/test/infrastructure/DummyContext.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2021, Danilo Pianini and contributors listed in the project's build.gradle.kts or pom.xml file.
+ * Copyright (C) 2023, Danilo Pianini and contributors listed in the project's build.gradle.kts file.
  *
  * This file is part of Protelis, and is distributed under the terms of the GNU General Public License,
  * with a linking exception, as described in the file LICENSE.txt in this project's top directory.
@@ -34,7 +34,7 @@ public final class DummyContext extends AbstractExecutionContext<DummyContext> {
         @Override
         public String toString() {
             return "DummyUID";
-        };
+        }
     };
     private final Random rng = new Random(0);
 
