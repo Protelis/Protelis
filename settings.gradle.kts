57fff2e04e117de7ae0a58214975bfc069ee7be4
Danilo Pianini <danilo.pianini@unibo.it>
2024-04-09 20:48:27 +0200

    build: switch from Gradle Enterprise to Develocity

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -13,16 +13,16 @@ include(
 )
 
 plugins {
-    id("com.gradle.enterprise") version "3.17.1"
+    id("com.gradle.develocity") version "3.17.1"
     id("org.danilopianini.gradle-pre-commit-git-hooks") version "2.0.4"
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
 }
 
-gradleEnterprise {
+develocity {
     buildScan {
-        termsOfServiceUrl = "https://gradle.com/terms-of-service"
-        termsOfServiceAgree = "yes"
-        publishAlways()
+        termsOfUseUrl = "https://gradle.com/terms-of-service"
+        termsOfUseAgree = "yes"
+        uploadInBackground = !System.getenv("CI").toBoolean()
     }
 }
 
