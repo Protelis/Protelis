a248696e64f432527e70bb86ce163809b5cb285b
Danilo Pianini <danilo.pianini@gmail.com>
2025-06-09 10:46:33 +0200

    test(test): port void-return test to Kotlin

---
+++ protelis-test/src/test/kotlin/org/protelis/test/VoidReturnTest.kt
@@ -0,0 +1,13 @@
+package org.protelis.test
+
+import org.junit.Test
+
+/**
+ * Test calling a void method.
+ */
+class VoidReturnTest {
+    @Test
+    fun unit01() {
+        ProgramTester.runFile("/unit01.pt")
+    }
+}
