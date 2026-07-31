25b813fca99f5c68378e4f33ae3f7afbbfa58918
Danilo Pianini <danilo.pianini@gmail.com>
2026-07-31 10:48:23 +0200

    build: remove unnecessary suppression for DSL scope violation in build.gradle.kts

--- protelis-lang/build.gradle.kts
+++ protelis-lang/build.gradle.kts
@@ -4,8 +4,6 @@
  * This file is part of Protelis, and is distributed under the terms of the GNU General Public License,
  * with a linking exception, as described in the file LICENSE.txt in this project's top directory.
  */
-
-@Suppress("DSL_SCOPE_VIOLATION")
 plugins {
     alias(libs.plugins.protelisdoc)
 }
